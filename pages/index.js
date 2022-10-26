2f615b5233e8505c1d8ccca4e4354a339abce3d1
htetaunglinn1998 <htetaung.linn@smilaxglobal.com>
2022-10-26 16:39:01 +0630

    Next Js and Tailwind Added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <h1 className="text-3xl font-bold underline italic text-center text-teal-400">
+        Welcome to Knight of Honor!
+      </h1>
+    </>
   )
 }
 
--- pages/product/swater.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const swater = () => {
-  return (
-    <div>Landing page for swaters</div>
-  )
-}
-
-export default swater
