1e497e4790c6443655c5c796b493613ea86cbba8
Htet Aung Linn <linnwilliam35@gmail.com>
2022-11-01 22:16:15 +0630

    To pull

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Image from 'next/image'
 
 const Home = () => {
   return (
-    <>
+    <div >
       <h1 className="text-3xl font-bold underline italic text-center text-teal-400">
         Welcome to Knight of Honor!
       </h1>
-    </>
+    </div>
   )
 }
 
