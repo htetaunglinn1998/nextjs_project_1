ed346b17fa88ae8fda6ceaf66f8bec5731583fc3
htetaunglinn1998 <htetaung.linn@smilaxglobal.com>
2022-11-04 16:20:28 +0630

    Just small touch

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,30 @@ const Home = () => {
         </ul>
       </nav>
       <main>
-        <div className='hero-section tracking-widest'>
+        <div id='hero-section' className='hero-section tracking-widest overflow-hidden'>
           <h1 className=' text-center text-white font-bold text-8xl'>
             CREATIVE <br />
             DEVELOPER
           </h1>
-          <Image width={500} height={500} src="https://assets.website-files.com/6310ffd0c310a8603c82807d/63110b34132eca9f79d3863b_hm-hero.webp"></Image>
+          <div className='flex justify-center -m-7 overflow-hidden'>
+            <Image width={450} height={500} src="https://assets.website-files.com/6310ffd0c310a8603c82807d/63110b34132eca9f79d3863b_hm-hero.webp" objectFit='cover'></Image>
+          </div>
+          <div className='flex justify-between mx-7 text-sm '>
+            <p className='text-gray-400'>DESIGN STUDIO <br /> AVAILABLE FOR WORK</p>
+            <p className='text-gray-400 text-right'>9081 LAKEWOOD <br /> GARDENS JUNCTION</p>
+          </div>
+        </div>
+
+        <div className=''>
+          <div>
+            <p>3D COMMERCIAL</p>
+            <p>SPRING HARMONY</p>
+            <p>Morbi dolor nulla tellus sit dignissim tortor, sed. Eget maecenas suspendisse.</p>
+            <a className='text-white'>VIEW LIVE WALLPAPER</a>
+          </div>
+          <div>
+
+          </div>
         </div>
       </main>
     </div>
