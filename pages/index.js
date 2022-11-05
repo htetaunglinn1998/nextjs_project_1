e1fd4795c92f1d09d8c85ca26da4a79a0153f609
Htet Aung Linn <linnwilliam35@gmail.com>
2022-11-05 12:52:39 +0630

    To pull latest

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,17 @@ const Home = () => {
       </nav>
       <main>
         <div className='hero-section tracking-widest'>
-          <h1 className=' text-center text-white font-bold text-8xl'>
+          <h1 className='text-center text-white font-bold text-8xl'>
             CREATIVE <br />
             DEVELOPER
           </h1>
-          <Image width={500} height={500} src="https://assets.website-files.com/6310ffd0c310a8603c82807d/63110b34132eca9f79d3863b_hm-hero.webp"></Image>
+          <div className='flex justify-center -m-7 overflow-hidden'>
+            <Image width={500} height={600} className="object-cover" src="https://assets.website-files.com/6310ffd0c310a8603c82807d/63110b34132eca9f79d3863b_hm-hero.webp"></Image>
+          </div>
+          <div className='flex justify-between px-5 text-sm leading-4'>
+            <p className='text-white'>Web Developer <br /> Available for work</p>
+            <p className='text-white text-right'>North Okkalar <br /> Yangon</p>
+          </div>
         </div>
       </main>
     </div>
